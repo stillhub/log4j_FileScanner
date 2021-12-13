8a673b6c013551eafcb2f1a632d13f802feaa74d
stillhub <75730966+stillhub@users.noreply.github.com>
2021-12-14 11:09:52 +1300

    Add files via upload

---
+++ Log4j_Scanner.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module ActiveDirectory
+
+$ServerLog = "C:\temp\Servers_Log4j.csv"
+
+$ServerList = Get-ADComputer -Filter 'operatingsystem -like "*server*" -and enabled -eq "true"' | Select Name
+#$ServerList = Get-Content ".\Servers.txt"
+
+foreach($Server in $ServerList){
+    Invoke-Command -ComputerName $SERVER.Name -ScriptBlock {get-childitem C:\log4j*.jar -Recurse} | Export-CSV $ServerLog -notypeinformation -append
+
+}
