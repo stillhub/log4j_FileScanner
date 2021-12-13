6e70a0c141bb092a69d166f85de22fdabfe4cdd1
stillhub <75730966+stillhub@users.noreply.github.com>
2021-12-14 11:29:50 +1300

    Update Log4j_Scanner.ps1

--- Log4j_Scanner.ps1
+++ Log4j_Scanner.ps1
@@ -1,7 +1,8 @@
-﻿Import-Module ActiveDirectory
+Import-Module ActiveDirectory
 $ServerLog = "C:\temp\Servers_Log4j.csv"
 $ServerList = Get-ADComputer -Filter 'operatingsystem -like "*server*" -and enabled -eq "true"' | Select Name
 
 foreach($Server in $ServerList){
+    Write-Host "Scanning:" $SERVER.Name
     Invoke-Command -ComputerName $SERVER.Name -ScriptBlock {get-childitem C:\log4j*.jar -Recurse} | Export-CSV $ServerLog -notypeinformation -append
 }
