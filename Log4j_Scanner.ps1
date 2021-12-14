b3ae169febdf9c5beb06b415ff94703d4c4710a2
stillhub <75730966+stillhub@users.noreply.github.com>
2021-12-14 14:59:43 +1300

    Update Log4j_Scanner.ps1

--- Log4j_Scanner.ps1
+++ Log4j_Scanner.ps1
@@ -3,6 +3,7 @@ $ServerLog = "C:\temp\Servers_Log4j.csv"
 $ServerList = Get-ADComputer -Filter 'operatingsystem -like "*server*" -and enabled -eq "true"' | Select Name
 
 foreach($Server in $ServerList){
-    Write-Host "Scanning:" $SERVER.Name
-    Invoke-Command -ComputerName $SERVER.Name -ScriptBlock {get-childitem C:\log4j*.jar -Recurse} | Export-CSV $ServerLog -notypeinformation -append
+    Write-Host "Scanning: " $SERVER.name
+    Invoke-Command -ComputerName $SERVER.Name -ScriptBlock {$DriveList = (Get-PSDrive -PSProvider FileSystem).Root; foreach($Drive in $DriveList){$Term = $Drive + "log4j*.jar"; get-childitem $Term -Recurse}} | Export-CSV $ServerLog -notypeinformation -append
+    
 }
